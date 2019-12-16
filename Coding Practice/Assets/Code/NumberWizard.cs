774d2b6b17ae67bcacf04774cec058c49a5644f8
NatPlatt <n.zollinger26@hotmail.com>
2019-12-16 11:52:39 -0700

    writing number wizard code

---
+++ Coding Practice/Assets/Code/NumberWizard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+	public float largeNum = 1000;
+
+	public float smallNum = 1;
+	
+	void Start () {
+		Debug.Log("Welcome to the Number Wizard.");
+		Debug.Log("The Highest number is": largeNum);
+		Debug.Log("The lowester number is ", smallNum);
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
