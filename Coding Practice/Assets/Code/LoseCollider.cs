cc46aa50b958d46fc032094e6ff092e16c92b199
NatPlatt <n.zollinger26@hotmail.com>
2019-12-27 13:38:53 -0700

    writing script for ball breaker game

---
+++ Coding Practice/Assets/Code/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+
+	public string sceneName;
+	private void OnTriggerEnter(Collider other)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
