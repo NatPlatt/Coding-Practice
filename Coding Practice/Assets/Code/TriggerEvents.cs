78094d1298764362387071197421de2b9a6d935b
NatPlatt <n.zollinger26@hotmail.com>
2019-12-30 15:34:56 -0700

    writing trigger events script
    so that each time the block is hit, the total number goes down 1

---
+++ Coding Practice/Assets/Code/TriggerEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvents : MonoBehaviour
+{
+	public float hitNum;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		hitNum -= 1;
+	}
+}
