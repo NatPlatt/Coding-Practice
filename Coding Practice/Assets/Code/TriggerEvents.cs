0b087aa47ab4031e8dece6bc98d95de78dcb3c58
NatPlatt <n.zollinger26@hotmail.com>
2019-12-31 13:06:49 -0700

    added to trigger events script
    so that when the block gets hit a certain number of times, it disappears from game view

--- Coding Practice/Assets/Code/TriggerEvents.cs
+++ Coding Practice/Assets/Code/TriggerEvents.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 public class TriggerEvents : MonoBehaviour
 {
 	public float hitNum;
-	
+	public GameObject block;
 	private void OnTriggerEnter(Collider other)
 	{
 		hitNum -= 1;
+	}
+
+	private void TurnOff()
+	{
+		if (hitNum == 0)
+		{
+			block.gameObject.SetActive(false);
+		}
+	}
+
+	private void Update()
+	{
+		TurnOff();
 	}
 }
