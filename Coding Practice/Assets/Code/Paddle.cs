08a5d7a1924237d9fb6e0c5e04b3115a251961d6
NatPlatt <n.zollinger26@hotmail.com>
2020-01-03 13:53:21 -0700

    Update Paddle.cs

--- Coding Practice/Assets/Code/Paddle.cs
+++ Coding Practice/Assets/Code/Paddle.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-
+	[SerializeField] private float minX = 1f;
+	[SerializeField] private float maxX = 1f;
 	[SerializeField] private float screenWidth;
 	
 	void Update () {
 		float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidth;
 		Vector3 paddlePos = new Vector3(mousePosInUnits, transform.position.y);
+		paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
 		transform.position = paddlePos;
 	}
 }
