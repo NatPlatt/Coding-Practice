0007bfb70d932c9b8e9d91931154d134d888586c
NatPlatt <n.zollinger26@hotmail.com>
2020-01-03 14:04:15 -0700

    paddle wouldn't move
    had to change values of serialized fields in editor

--- Coding Practice/Assets/Code/Paddle.cs
+++ Coding Practice/Assets/Code/Paddle.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-	[SerializeField] private float minX = 1f;
-	[SerializeField] private float maxX = 15f;
+	[SerializeField] private float minX = -5.5f;
+	[SerializeField] private float maxX = 5.5f;
 	[SerializeField] private float screenWidth;
 	
 	void Update () {
 		float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidth;
-		Vector3 paddlePos = new Vector3(mousePosInUnits, transform.position.y);
+		Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
 		paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
 		transform.position = paddlePos;
 	}
