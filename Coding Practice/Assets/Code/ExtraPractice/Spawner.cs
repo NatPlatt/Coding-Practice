75243ec9dfe96e920b5bc4ddd101f127743ac8d2
NatPlatt <n.zollinger26@hotmail.com>
2020-01-17 18:41:12 -0700

    writing spawn script

--- Coding Practice/Assets/Code/ExtraPractice/Spawner.cs
+++ Coding Practice/Assets/Code/ExtraPractice/Spawner.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class Spawner : MonoBehaviour
 {
 
 	public GameObject spawnObj;
 	private int angle = 15;
-	
+	public int num = 1;
 
 	private void Start()
 	{
-		private Vector3 spawnPosition = transform.position;
-		
+		//private Vector3 spawnPosition = transform.position;
+		if (num <= 5)
+		{
+			num = num + 1;
+			
+		}
+		Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+		var spawnPosition = transform.position + direction * 2;
+		Instantiate(spawnObj, spawnPosition, Quaternion.identity);
 	}
 }
