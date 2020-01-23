3b408ff946c3380c612e02e092f68192857bc858
NatPlatt <n.zollinger26@hotmail.com>
2020-01-23 12:33:20 -0700

    put unity event in on state enter function

--- Coding Practice/Assets/Code/CharDev/animBehavior.cs
+++ Coding Practice/Assets/Code/CharDev/animBehavior.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class animBehavior : StateMachineBehaviour
 {
+
+    public UnityEvent stateEnterEvent;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log(stateInfo);
+        stateEnterEvent.Invoke();
     }
 }
