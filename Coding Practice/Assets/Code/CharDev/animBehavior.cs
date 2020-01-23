b14f67892c433a64cd8121e2e00a78b248b01671
NatPlatt <n.zollinger26@hotmail.com>
2020-01-23 12:24:47 -0700

    made animation

---
+++ Coding Practice/Assets/Code/CharDev/animBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animBehavior : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log(stateInfo);
+    }
+}
