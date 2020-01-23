880083e9d49f3867d29f5f3ad13f5a40281f79a0
NatPlatt <n.zollinger26@hotmail.com>
2020-01-23 12:39:40 -0700

    wrote debugger script

---
+++ Coding Practice/Assets/Code/CharDev/Debugger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Debugger : ScriptableObject {
+
+	public void OnDebug(string message)
+	{
+		Debug.Log("");
+	}
+}
+
+[CreateAssetMenu]
+
+public class GameAction : ScriptableObject
+{
+	public UnityAction action;
+
+	public void Raise()
+	{
+		if (action != null) action.Invoke();
+	}
+}
