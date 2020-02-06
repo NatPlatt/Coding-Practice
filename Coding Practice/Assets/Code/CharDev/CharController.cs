656b3d3d3fb2c5c5a1a502f05eff0a0760145010
NatPlatt <n.zollinger26@hotmail.com>
2020-02-06 12:15:06 -0700

    Update CharController.cs

--- Coding Practice/Assets/Code/CharDev/CharController.cs
+++ Coding Practice/Assets/Code/CharDev/CharController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharController : MonoBehaviour
 {
+	private CharacterController charController;
+	
 	public float speed = 5.0f;
 	public float jumpSpeed = 8.0f;
 	public float gravity = 20f;
 	
 	private Vector3 moveDirection = Vector3.zero;
 	
-	void Start () {
-		
+	void Start ()
+	{
+		charController = GetComponent<CharacterController>();
 	}
 	
 	
