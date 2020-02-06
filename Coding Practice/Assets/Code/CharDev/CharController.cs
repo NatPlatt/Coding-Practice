855f71c16c9c47fc7e718ee570a57cb4d67ba05c
NatPlatt <n.zollinger26@hotmail.com>
2020-02-06 12:13:30 -0700

    writing character controller script
    to test another way to control the character compared to what I already have

---
+++ Coding Practice/Assets/Code/CharDev/CharController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20f;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
